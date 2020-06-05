69fe707f1af4f6e53cb826f5975ef169f7aaed4a
zhangxiaotin <zhangxiaotin@126.com>
2020-06-05 19:29:14 +0800

    test

--- src/store/mutationTypes.js
+++ src/store/mutationTypes.js
@@ -1,6 +1,7 @@
 //用户信息
 export const CHANGE_USERINFO = "CHANGE_USERINFO";
 //游戏名列表
-export const CHANGE_GAMELIST = "CHANGE_GAMELIST";
+// export const CHANGE_GAMELIST = "CHANGE_GAMELIST";
 
+//const CHANGE_PAGE = "CHANGE_PAGE";
 //const CHANGE_PAGE = "CHANGE_PAGE";
