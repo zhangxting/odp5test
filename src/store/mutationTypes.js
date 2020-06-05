fe92e3c44151054506acab2ad17cd897ccfc8489
zhangxiaotin <zhangxiaotin@126.com>
2020-06-05 19:34:03 +0800

    test

--- src/store/mutationTypes.js
+++ src/store/mutationTypes.js
@@ -1,14 +1,8 @@
 //用户信息
 export const CHANGE_USERINFO = "CHANGE_USERINFO";
 
-//sdcfdsfadsfsdfsd
-// dafdsfdsa
 //游戏名列表
 export const CHANGE_GAMELIST = "CHANGE_GAMELIST";
 
-//const CHANGE_PAGE = "CHANGE_PAGE";
-//const CHANGE_PAGE = "CHANGE_PAGE";
-//const CHANGE_PAGE = "CHANGE_PAGE";
-//const CHANGE_PAGE = "CHANGE_PAGE";
 //const CHANGE_PAGE = "CHANGE_PAGE";
 //const CHANGE_PAGE = "CHANGE_PAGE";
